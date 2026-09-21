40690120d23eb9db925d6ff99c18364292dea0e6
Mariozo <maris.ozo@gmail.com>
2026-09-21 23:15:50 +0300

    Fix canonical DB path syntax

--- ls_core/runtime.py
+++ ls_core/runtime.py
@@ -90,7 +90,8 @@ def initialize_runtime_environment():
 
 LS_UPDATE_PROBE_MARKER = "LS_UPDATE_PROBE_OK"
 
-LEGACY_DB_PATH = HOST_ROOT / "suno_finder_v4.db"\nDB_PATH = DATA_DIR / "local_suno.db"
+LEGACY_DB_PATH = HOST_ROOT / "suno_finder_v4.db"
+DB_PATH = DATA_DIR / "local_suno.db"
 
 LOCAL_INVENTORY_DB_PATH = DATA_DIR / "suno_local_inventory.db"
 
